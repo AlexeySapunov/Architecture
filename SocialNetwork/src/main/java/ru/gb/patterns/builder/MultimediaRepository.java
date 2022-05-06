406606236a60405e45f262bfaa90301cdda64251
Alexey <blacka8607@rambler.ru>
2022-05-06 13:49:47 +0300

    Homework to lesson 3

---
+++ SocialNetwork/src/main/java/ru/gb/patterns/builder/MultimediaRepository.java
@@ -0,0 +1,6 @@
+package ru.gb.patterns.builder;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MultimediaRepository extends JpaRepository<Multimedia, Long> {
+}
