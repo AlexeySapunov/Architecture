1c6f603cf9bf86f06b7257fe20954ba20f638f8f
Alexey <blacka8607@rambler.ru>
2022-05-12 11:43:50 +0300

    Homework to lesson 3 corrected

---
+++ SocialMedia/src/main/java/ru/gb/patterns/builder/MultimediaService.java
@@ -0,0 +1,10 @@
+package ru.gb.patterns.builder;
+
+public interface MultimediaService {
+
+    Long getId();
+    String getUsername();
+    String getContentType();
+    String getStorageFileName();
+    User getUser();
+}
